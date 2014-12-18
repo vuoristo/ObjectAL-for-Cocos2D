725dc000768acb3a1db9d561b9c2ab7423710c24
Risto Vuorio <risto@apportable.com>
2014-12-17 21:18:17 -0800

    Remove bridgekit protocol from OALAudioTrack and replace the callback with a block callback.

--- ObjectAL/ObjectAL/AudioTrack/OALAudioTrack.h
+++ ObjectAL/ObjectAL/AudioTrack/OALAudioTrack.h
@@ -48,8 +48,7 @@
 #if __CC_PLATFORM_IOS || __CC_PLATFORM_MAC
 @interface OALAudioTrack : NSObject <AVAudioPlayerDelegate,OALSuspendManager>
 #elif __CC_PLATFORM_ANDROID
-BRIDGE_CLASS("com.spritebuilder.OALAudioTrack")
-@interface OALAudioTrack : JavaObject <AndroidMediaPlayerOnCompletionListener,OALSuspendManager>
+@interface OALAudioTrack : JavaObject <OALSuspendManager>
 #endif
 {
     /** If true, this track is recording metering data */
